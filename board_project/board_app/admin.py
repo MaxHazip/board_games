34f4757e33785ef61f6b82b826e3af7bdbb4723f
Maks Hazip <robergorlfed@gmail.com>
2025-10-02 23:11:18 +0500

    first

---
+++ board_project/board_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(BoardGames)
+admin.site.register(Genres)
+admin.site.register(Categories)
+admin.site.register(Profiles)
+admin.site.register(BonusCards)
+admin.site.register(Orders)
+admin.site.register(Cart)
+admin.site.register(Purchased)
---
+++ board_project/board_app/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+
+
+# Create your models here.
+class BoardGames(models.Model):
+    name = models.CharField('Название игры', max_length=35, unique=True)
+    price = models.DecimalField('Цена', max_digits=20, decimal_places=2)
+    description = models.TextField('Описание игры')
+    players_number = models.CharField('Количество игроков', max_length=6)
+    game_time = models.CharField('Среднее время игры', max_length=30)
+    min_age = models.IntegerField('Минимальный возраст')
+    games_genres = models.ManyToManyField('Genres')
+    games_categories = models.ManyToManyField('Categories')
+
+
+    class Meta:
+        verbose_name = 'Настольная игра'
+        verbose_name_plural = 'Настольные игры'
+
+    def __str__(self):
+        return f"{self.name} {self.price}"
+
+class Genres(models.Model):
+    name = models.CharField('Название жанра', max_length=35, unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class Categories(models.Model):
+    name = models.CharField('Название', max_length=30, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class Profiles(models.Model):
+    first_name = models.CharField('Имя', max_length=40)
+    surname = models.CharField('Фамилия', max_length=40)
+    middle_name = models.CharField('Отчество', max_length=40, null=True)
+    email = models.EmailField('Электронная почта', max_length=50, null=True)
+    phone_number = models.CharField('Телефон', max_length=50, null=True)
+    birthday = models.DateField('Дата рождения')
+    bonus_card_id = models.ForeignKey('BonusCards', verbose_name='Бонусная карта', on_delete=models.CASCADE)
+    order = models.ManyToManyField('BoardGames', through='Orders', through_fields=('profile_id', 'board_game_id'))
+    cart_items = models.ManyToManyField('BoardGames', through='Cart', through_fields=('profile_id', 'board_game_id'), related_name='in_cart')
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class BonusCards(models.Model):
+    balance = models.IntegerField('Баланс')
+    percentage_of_return = models.CharField('Процент возврата', max_length=3)
+    amount_of_purchases = models.DecimalField('Сумма покупок', max_digits=50, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Бонусная карта'
+        verbose_name_plural = 'Бонусные карты'
+
+    def __str__(self):
+        return f"{self.id}"
+
+class Orders(models.Model):
+    profile_id = models.ForeignKey('Profiles', verbose_name='Профиль', on_delete=models.CASCADE)
+    board_game_id = models.ForeignKey('BoardGames', verbose_name='Настольная игра', on_delete=models.CASCADE)
+    amount = models.IntegerField('Количество')
+
+class Cart(models.Model):
+    profile_id = models.ForeignKey('Profiles', verbose_name='Профиль', on_delete=models.CASCADE)
+    board_game_id = models.ForeignKey('BoardGames', verbose_name='Настольная игра', on_delete=models.CASCADE)
+    amount = models.IntegerField('Количество')
+
+class Purchased(models.Model):
+    profile_id = models.ForeignKey('Profiles', verbose_name='Профиль', on_delete=models.CASCADE)
+    board_game_id = models.ForeignKey('BoardGames', verbose_name='Настольная игра', on_delete=models.CASCADE)
+    amount = models.IntegerField('Количество')
+    cost = models.DecimalField('Сумма', max_digits=50, decimal_places=2)
+    bonus_amount = models.IntegerField('Количество бонусов')
+
+    class Meta:
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
+
+    def __str__(self):
+        return f"{self.id}"
