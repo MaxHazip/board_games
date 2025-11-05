75f01738e6a61bc6817dcfdb5fb6385715305c07
Студент группы IS <is@sielom.local>
2025-11-05 10:34:34 +0500

    id

--- board_project/board_app/urls.py
+++ board_project/board_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.root),
-    path('game-page', views.page)
+    path('game-pages/<int:item_id>', views.page)
 ]
--- board_project/board_app/views.py
+++ board_project/board_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -13,9 +13,11 @@ def root(request):
     }
     return render(request, 'main-content.html', context)
 
-def page(request):
+def page(request, item_id: int):
+    game = BoardGames.objects.filter(id=item_id)
+    get_object_or_404(game)
 
     context = {
-
+        'game': game,
     }
     return render(request, 'game-page.html', context)
